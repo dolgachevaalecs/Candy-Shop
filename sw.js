2d6be06129273d8309bff0eed64d0c5170cd4405
dolgachevaalecs <dolgachevaalecs@gmail.com>
2025-07-18 21:21:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,31 @@
 const CACHE_NAME = 'candy-shop-cache-v1';
 const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/main.css',
-    '/choco.jpg',
-    '/strawb.png',
-    '/cake.png',
-    '/byket.png',
-    '/sweets.png',
-    '/home.png',
-    '/logo1.png'
+  '/',
+  '/index.html',
+  '/css/main.css',
+  '/img/choco.jpg',
+  '/img/strawb.png',
+  '/img/cake.png',
+  '/img/byket.png',
+  '/img/sweets.png',
+  '/img/home.png',
+  '/img/korp.jpg',
+  '/img/11.jpg',
+  '/img/12.jpg',
+  '/img/13.jpg',
+  '/img/14.jpg'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
